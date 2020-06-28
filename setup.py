7fac22104b742619daf2b7f57e2c8312b2f49f16
Phil Wang <lucidrains@gmail.com>
2020-06-28 09:21:26 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'linformer',
   packages = find_packages(),
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'Linformer implementation in Pytorch',
   author = 'Phil Wang',
